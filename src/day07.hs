288a994eae34f135329357d737b1f2a35f2f2f64
Ryan Barouki <ryanbarouki@gmail.com>
2025-12-07 23:42:59 +0000

    optimise day 7

--- src/day07.hs
+++ src/day07.hs
@@ -8,6 +8,8 @@ type PathCount = HM.HashMap (Int, Int) Int
 
 type Point = (Int, Int)
 
+type Frontier = S.Set Point
+
 main :: IO ()
 main = do
   input <- getContents
@@ -19,75 +21,58 @@ main = do
   putStrLn $ "Part 1: " ++ result1 ++ " | time: " ++ show t1 ++ " s"
   putStrLn $ "Part 2: " ++ result2 ++ " | time: " ++ show t2 ++ " s"
 
-advanceFrontier :: [Point] -> [Point]
-advanceFrontier = map (\(x, y) -> (x, y + 1))
+fst' (x, _, _) = x
 
-updateFrontier :: Grid -> [Point] -> (S.Set Point, Int)
-updateFrontier grid = foldl foldf (S.empty, 0)
-  where
-    foldf (acc, c) p@(x, y)
-      | grid HM.! p == '^' =
-          ( S.insert (x + 1, y) (S.insert (x - 1, y) acc),
-            c + 1
-          )
-      | otherwise = (S.insert p acc, c)
+snd' (_, x, _) = x
+
+thd' (_, _, x) = x
+
+advanceFrontier :: Frontier -> Frontier
+advanceFrontier = S.map (\(x, y) -> (x, y + 1))
 
 part1 :: String -> String
-part1 input = show $ (\(_, _, c, _) -> c) . last $ takeWhile (\(_, _, _, done) -> not done) $ iterate step (g, frontier, 0, False)
+part1 input = show . thd' . last . takeWhile (not . done . snd') $ iterate step (g, frontier, 0)
   where
     g = enumerateHM input :: Grid
-    frontier = filter (\p -> g HM.! p == 'S') (HM.keys g)
+    frontier = S.fromList $ filter (\p -> g HM.! p == 'S') (HM.keys g)
+    maxY = maximum $ map snd (HM.keys g)
+    done = (> maxY - 1) . snd . S.findMin
 
-step :: (Grid, [Point], Int, Bool) -> (Grid, [Point], Int, Bool)
-step (grid, frontier, count, _)
-  | done (advanceFrontier frontier) = (grid, frontier, count, True)
-  | otherwise = (grid, frontier', count + count', False)
+step :: (Grid, Frontier, Int) -> (Grid, Frontier, Int)
+step (grid, frontier, count) = (grid, frontier', count + count')
   where
-    (frontier', count') = (\(a, b) -> (S.toList a, b)) . stepFrontier $ frontier
+    (frontier', count') = stepFrontier frontier
     stepFrontier = updateFrontier . advanceFrontier
     updateFrontier = foldl foldf (S.empty, 0)
-
-    foldf (acc, c) p@(x, y) =
-      if grid HM.! p == '^'
-        then
-          ( S.insert (x + 1, y) $
-              S.insert (x - 1, y) acc,
-            c + 1
-          )
-        else (S.insert p acc, c)
-
-    maxY = maximum $ map snd (HM.keys grid)
-    done = any (\(x, y) -> y > maxY)
+    foldf (acc, c) p@(x, y)
+      | grid HM.! p == '^' = (S.insert (x + 1, y) $ S.insert (x - 1, y) acc, c + 1)
+      | otherwise = (S.insert p acc, c)
 
 part2 :: String -> String
 part2 input = show $ foldl (\acc p -> acc + pc' HM.! p) 0 finalFrontier
   where
-    (pc', finalFrontier) = (\(_, f, c, _) -> (c, f)) . last $ takeWhile (\(_, _, _, done) -> not done) $ iterate step2 (g, frontier, pathCount, False)
+    (finalFrontier, pc') = (\(_, f, c) -> (f, c)) . last . takeWhile (not . done . snd') $ iterate step2 (g, frontier, pathCount)
     g = enumerateHM input :: Grid
-    frontier = filter (\p -> g HM.! p == 'S') (HM.keys g)
-    pathCount = HM.insert (head frontier) 1 $ foldl (\acc p -> HM.insert p 0 acc) HM.empty (HM.keys g)
+    start = head $ filter (\p -> g HM.! p == 'S') (HM.keys g)
+    frontier = S.fromList [start]
+    pathCount = HM.insert start 1 $ HM.map (const 0) g
+    maxY = maximum $ map snd (HM.keys g)
+    done = (> maxY - 1) . snd . S.findMin
 
-step2 :: (Grid, [Point], PathCount, Bool) -> (Grid, [Point], PathCount, Bool)
-step2 (grid, frontier, pathCount, _)
-  | done (advanceFrontier frontier) = (grid, frontier, pathCount, True)
-  | otherwise = (grid, frontier', pc', False)
+step2 :: (Grid, Frontier, PathCount) -> (Grid, Frontier, PathCount)
+step2 (grid, frontier, pathCount) = (grid, frontier', pc')
   where
-    (pc', frontier') = (\(a, b) -> (a, S.toList b)) . stepFrontier $ frontier
+    (pc', frontier') = stepFrontier frontier
     stepFrontier = updateFrontier . advanceFrontier
     updateFrontier = foldl foldf (pathCount, S.empty)
 
     foldf (pc, acc) p@(x, y) = case grid HM.! p of
       '^' ->
-        ( addPathCount (x + 1, y) oldCount $
-            addPathCount (x - 1, y) oldCount pc,
-          S.insert (x + 1, y) $
-            S.insert (x - 1, y) acc
+        ( addPathCount (x + 1, y) oldCount $ addPathCount (x - 1, y) oldCount pc,
+          S.fromList [(x - 1, y), (x + 1, y)] <> acc
         )
       '.' -> (addPathCount p oldCount pc, S.insert p acc)
       where
         oldCount = pc HM.! (x, y - 1)
 
-    addPathCount p val pc = HM.insert p (pc HM.! p + val) pc
-
-    maxY = maximum $ map snd (HM.keys grid)
-    done = any (\(x, y) -> y > maxY)
+    addPathCount p val = HM.adjust (+ val) p
