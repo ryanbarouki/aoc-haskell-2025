4a718b31cb0519eae1b6c56d141a2e0a2044c15c
Ryan Barouki <ryanbarouki@gmail.com>
2025-12-10 22:59:07 +0000

    day 10 -- finally

--- src/day10.hs
+++ src/day10.hs
@@ -1,5 +1,6 @@
 import AOC
 import Data.Bits
+import Data.SBV
 import Data.Set qualified as S
 
 main :: IO ()
@@ -7,8 +8,9 @@ main = do
   input <- getContents
   (result1, t1) <- timeCpu . evaluate . force . part1 $ input
   putStrLn $ "Part 1: " ++ result1 ++ " | time: " ++ show t1 ++ " s"
-  (result2, t2) <- timeCpu . evaluate . force . part2 $ input
-  putStrLn $ "Part 2: " ++ result2 ++ " | time: " ++ show t2 ++ " s"
+
+  (result2, t2) <- timeCpu $ part2 input
+  putStrLn $ "Part 2: " ++ show result2 ++ " | time: " ++ show t2 ++ " s"
 
 type Lights = Int -- Binary
 
@@ -16,20 +18,7 @@ type Buttons = [Int] -- Binary
 
 type Joltages = [Int]
 
--- [.#...#] (2,4) (3,4) (0,2,3,5) (0,1,2,3) (1,3,4,5) {26,32,28,47,23,20}
--- parser :: Parser Lights
--- parser = do
---   char '['
---   lightStr <- some (char '.' <|> char '#')
---   let lights = map (== '#') lightStr
---   char ']'
---   space
---   char '('
---   button <- sepBy (some digitChar) (char ',')
---   return $ binToDec lights
-
 part1 :: String -> String
--- part1 input = show $ map (parse parser) (lines input)
 part1 input = show $ sum $ map p (lines input)
   where
     -- p :: String -> (Lights, Buttons)
@@ -53,31 +42,43 @@ bfs neighbours end finished ((f, acc) : frontier)
   where
     filtered = zip (filter (\n -> n `S.notMember` finished && n `notElem` map fst frontier) $ neighbours f) (repeat (acc + 1))
 
--- bfs _ _ acc _ [] = acc
-
 neighbours :: Buttons -> Lights -> [Lights]
 neighbours bs l = map (`xor` l) bs
 
--- solve1 :: (Lights, Buttons)
+-- PART 2
+problem nButtons constraints = do
+  ns <- sequence $ map sInteger ["n" ++ show i | i <- [0 .. nButtons - 1]]
 
-part2 :: String -> String
-part2 input = show $ sum $ map p ([head $ lines input])
-  where
-    -- p :: String -> (Lights, Buttons)
-    p l = solve2 joltages buttons
-      where
-        split = splitOn " " l
-        lights :: Int = binToDec $ reverse $ map (== '#') (init $ tail $ head split)
-        joltages = numbers' $ last split
+  mapM_ constrain [n .>= 0 | n <- ns]
 
-        buttons = map (\l -> head $ map numbers' (splitOn ")" l)) (init $ tail split)
+  mapM_ (\(j, bIndices) -> constrain $ (sum $ map (\i -> ns !! i) bIndices) .== literal (fromIntegral j)) constraints
 
-solve2 finalJoltages bs = bfs (neighbours2 bs finalJoltages) (== finalJoltages) S.empty [(replicate (length finalJoltages) 0, 0)]
+  minimize "steps" $ sum ns
 
--- neighbours2 :: [[Int]] -> Joltages -> [Joltages]
-neighbours2 bs finalJoltages js = filteredJoltages
-  where
-    filteredJoltages = filter (\njs -> all (\(finalJ, j) -> j <= finalJ) (zip finalJoltages njs)) nextJoltages
-    nextJoltages = map (press js) bs
-    press :: Joltages -> [Int] -> Joltages
-    press js b = map (\(i, jolt) -> if i `elem` b then jolt + 1 else jolt) (zip [0 ..] js) :: Joltages
+processLine :: String -> IO Int
+processLine l = do
+  let splitLine = splitOn " " l
+
+  let buttonsStr = init $ tail splitLine
+      joltagesStr = last splitLine
+
+      buttonMap :: [[Int]]
+      buttonMap = map (\s -> numbers' (head $ splitOn ")" s)) buttonsStr
+
+      nButtons = length buttonMap
+
+      joltages :: [Int] = numbers' joltagesStr
+
+      buttonIndices valIdx = filter (\i -> valIdx `elem` (buttonMap !! i)) [0 .. nButtons - 1]
+
+      constraints = zip joltages [buttonIndices valIdx | valIdx <- [0 .. length joltages - 1]]
+
+  let problemGoal = problem nButtons constraints
+
+  result <- optimize Lexicographic problemGoal
+  let strResult = show result
+  return $ head $ numbers' $ last (lines strResult)
+
+part2 :: String -> IO Int
+part2 input = do
+  foldM (\acc l -> processLine l >>= (\i -> return $ acc + i)) 0 (lines input)
